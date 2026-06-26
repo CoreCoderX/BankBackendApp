1f887c719d521bb26ce16f3251679fa7d8bd2702
Yashwanth <yashwanth4625@gmail.com>
2026-06-26 17:37:00 +0530

    testing done for LoanApply ,admin loan approval&reject

--- src/main/java/com/dvein/banking_backend/loan/controller/LoanAdminController.java
+++ src/main/java/com/dvein/banking_backend/loan/controller/LoanAdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/admin/loan")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN')")
 public class LoanAdminController {
 
     private final LoanDisbursementService loanDisbursementService;
